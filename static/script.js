edea5f70d4bf7a142ecf83da35bd3e310e05c17d
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-04-06 18:25:45 +0600

    buttons added, so functions are modified

--- static/script.js
+++ static/script.js
@@ -1,6 +1,3 @@
-alert(hideSplashScreen());
-// alert(showSplashScreen());
-
 function hideSplashScreen() {
     let noScroll = document.querySelector(".page-splash");
     noScroll.classList.remove("page-splash");
@@ -9,37 +6,35 @@ function showSplashScreen() {
     let noScroll = document.querySelector("#page-splash");
     noScroll.classList.add("page-splash");
 }
-
-let postElement  = document.createElement('p');
-postElement.innerHTML = "some post text";
-
-alert(createPostElement(postElement));
 function createPostElement(post) {
     document.getElementsByClassName("posts")[0].append(post);
-    console.log(post);
 }
-
-let userLink = document.createElement('a');
-document.createAttribute("class");
-userLink.setAttribute("href", "#");
-userLink.setAttribute("class", "muted");
-userLink.innerHTML = "username";
-let pTag = document.createElement('p');
-let pText = "some text";
-pTag.innerHTML = pText;
-let comBlock = document.createElement('div');
-
-alert(addCommentElement(createCommentElement(comBlock)));
-function createCommentElement(comment) {
-    comment.setAttribute("class", "py-2 pl-3");
-    comment.append(userLink);
-    comment.append(pTag);
-    // document.getElementsByClassName("comments")[0].append(comBlock);
-    console.log(userLink);
-    console.log(pTag);
-    console.log(comment);
+function createPostObject() {
+    let content = document.querySelector('[name=post]').value;
+    if (content == undefined || content.trim().length == 0)
+        return;
+    let postElement  = document.createElement('p');
+    postElement.innerHTML = content;
+    return postElement;
+}
+function createCommentObject() {
+    let userLink = document.createElement('a');
+    document.createAttribute("class");
+    userLink.setAttribute("href", "#");
+    userLink.setAttribute("class", "muted");
+    userLink.innerHTML = "username";
+    let pTag = document.createElement('p');
+    let content = document.querySelector('[name=comment]').value;
+    pTag.innerHTML = content;
+    let comBlock = document.createElement('div');
+    comBlock.append(userLink);
+    comBlock.append(pTag);
     return comBlock;
 }
+function createCommentElement(comment) {
+    comment.setAttribute("class", "py-2 pl-3");
+    return comment;
+}
 function addCommentElement(commentElemet) {
-    document.getElementsByClassName("comments")[0].append(comBlock);
+    document.getElementsByClassName("comments")[0].append(commentElemet);
 }
